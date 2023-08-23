a4b2cab9194b2a0331f6ebe91a905eacd48f7d15
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-08-23 19:23:03 +0300

    convert video to 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,33 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
+app.use(express.json());
 
-const port = '3000';
+app.post('/process-video', (req, res) => {
+  // TODO: Get path of the input video file from request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+  if (!inputFilePath) {
+    res.status(400).send('Bad Request: Missing input file path.');
+  } else if (!outputFilePath) {
+    res.status(400).send('Bad Request: Missing output file path.');
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360')
+    .on('end', () => {
+      res.status(200).send('Processing finished successfully.');
+    })
+    .on('error', (error) => {
+      console.log(`An error occurred: ${error.message}`);
+      res.status(500).send(`Internal Server Error: ${error.message}`);
+    })
+    .save(outputFilePath);
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`);
