b28caee1fdc2d1e3ddf17419a3a08fca141de4a1
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-08-26 10:35:42 +0300

    create firebase function to gnereate signedUrl for uploading videos

--- youtube-api-service/functions/src/index.ts
+++ youtube-api-service/functions/src/index.ts
@@ -1,11 +1,16 @@
+import { Storage } from '@google-cloud/storage';
 import { initializeApp } from 'firebase-admin/app';
 import { Firestore } from 'firebase-admin/firestore';
 import * as functions from 'firebase-functions';
 import * as logger from 'firebase-functions/logger';
+import { onCall } from 'firebase-functions/v2/https';
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = 'zoz-youtube-clone-raw-videos';
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -18,3 +23,34 @@ export const createUser = functions.auth.user().onCreate((user) => {
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
 });
+
+export const generateUploadUrl = onCall(
+  { maxInstances: 1 },
+  async (request) => {
+    // Check if the user is authenticated
+    // If the user is not authenticated request.auth is undefined.
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        'failed-precondition',
+        'The function must be called by authenticated user'
+      );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: 'v4',
+      action: 'write',
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return {
+      url,
+      fileName,
+    };
+  }
+);
